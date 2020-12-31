9075f27ee2b667eb42a9119abfd9268b2657db83
bosco <dmellobosco17@gmail.com>
2020-12-31 16:22:03 +0530

    second version

--- Scripts/MoreApps.cs
+++ Scripts/MoreApps.cs
@@ -7,7 +7,7 @@ public class MoreApps : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Version 1.2.0
     }
 
     // Update is called once per frame
